4085d365eeccbeb6d612747b23d5b7e9b01d32b2
JulienMellet <38491251+JulienMellet@users.noreply.github.com>
2018-11-21 15:32:00 +0100

    Working error function

--- basics.py
+++ basics.py
@@ -15,7 +15,6 @@ def calc_a_plus(mat):
     a_plus = inv(res_matmul)
     return a_plus
 
-
 def line_regression(main_mat, p):
     """
     Used to calcul the main elements of Ax*=b
@@ -72,10 +71,67 @@ def f_9(y_9,x):
     return y_9.item(0) + y_9.item(1)*x + y_9.item(2)*x**2 +  y_9.item(3)*x**3 +  y_9.item(4)*x**4 +  y_9.item(5)*x**5 + y_9.item(6)*x**6 + y_9.item(7)*x**7 + y_9.item(8)*x**8 + y_9.item(9)*x**9
 
 # Error between prediction and trained data
-def E(y_k, xt, yt):
+def E_1(y_k, xt, yt):
     res = 0
-    for i in range(xt):
-        res += (f_k(y_k, xt[i]) - yt[i])**2
+    for i in range(len(xt)):
+        res += (f_1(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_2(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_2(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_3(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_3(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_4(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_4(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_5(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_5(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_6(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_6(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_7(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_7(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_8(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_8(y_k, xt[i]) - yt[i])**2
+        print(res)
+    return res/2
+
+def E_9(y_k, xt, yt):
+    res = 0
+    for i in range(len(xt)):
+        res += (f_9(y_k, xt[i]) - yt[i])**2
+        print(res)
     return res/2
 
 ###############################################################################
@@ -91,10 +147,6 @@ for i in range(10):
     x_sinoise.append(i/9*2*np.pi)
 #print(y_sinoise)
 #print(x_sinoise)
-x_error = []
-for i in range(9):
-    x_error.append(i)
-print(x_error)
 
 ###############################################################################
 # Linear regression
@@ -107,6 +159,10 @@ y_1 = line_regression(m,1)
 y_2 = line_regression(m,2)
 y_3 = line_regression(m,3)
 y_4 = line_regression(m,4)
+y_5 = line_regression(m,5)
+y_6 = line_regression(m,6)
+y_7 = line_regression(m,7)
+y_8 = line_regression(m,8)
 y_9 = line_regression(m,9)
 
 plt.figure(1)
@@ -145,16 +201,22 @@ plt.title("Linear regression")
 # Error graph
 plt.figure(2)
 
-y_error = []
-for i in range 9
+x_error = []
+for i in range(8):
+    x_error.append(i+1)
 
-x = np.linspace(0, 2*np.pi, 100)
-plt.plot(x, abs(np.sin(x) - (f_3(y_3,x))), label='Error M = 3')
+y_error = [E_1(y_1, x_sinoise, y_sinoise), E_2(y_2, x_sinoise, y_sinoise), E_3(y_3, x_sinoise, y_sinoise), E_4(y_4, x_sinoise, y_sinoise), E_5(y_5, x_sinoise, y_sinoise), E_6(y_6, x_sinoise, y_sinoise), E_7(y_7, x_sinoise, y_sinoise), E_8(y_8, x_sinoise, y_sinoise), E_9(y_9, x_sinoise, y_sinoise)]
 
-plt.xlabel('x')
-plt.ylabel('y')
+for i in range(8):
+    y_error[i] = np.sqrt(2*y_error[i]/i)
 
-plt.title("error function")
+x = np.linspace(0, 9, 9)
+plt.plot(x, y_error, label='Training')
+
+plt.xlabel('M')
+plt.ylabel('Erms')
+
+plt.title("Error function")
 
 ###############################################################################
 # Regularization
@@ -173,4 +235,4 @@ plt.title("Regularization")
 
 plt.legend()
 
-#plt.show()
+plt.show()
