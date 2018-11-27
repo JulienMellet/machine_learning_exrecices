65247594b263095c200a06e38f4e71adb7d0ff04
JulienMellet <38491251+JulienMellet@users.noreply.github.com>
2018-11-27 14:27:34 +0100

    Massive change
    Error function working
    Regularization working
    basics file is for function
    main file run these functions

--- basics.py
+++ basics.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 #import scipy
 
-from numpy import matrix, matmul
+from numpy import matrix, matmul, array
 from numpy.linalg import inv
 
 def calc_a_plus(mat):
@@ -11,228 +11,99 @@ def calc_a_plus(mat):
     :type mat: matrix
     """
     mat_trans = mat.transpose()
-    res_matmul = matmul(mat_trans, mat)
+    res_matmul = matmul(mat_trans, mat) #Add here regularization term
     a_plus = inv(res_matmul)
     return a_plus
 
-def line_regression(main_mat, p):
+def line_regression(mat_M, p):
     """
     Used to calcul the main elements of Ax*=b
     :type main_mat: list
     :type p: int
     """
-    mat = []
-    vec = []
-    for elem in main_mat:
+    mat_A = []
+    vec_b = []
+
+    #for elem in range(len(mat_M)):
+    for elem in mat_M:
         tmp = []
         for j in range(p+1):
-            tmp.append(pow(elem[0], j))
-        mat.append(tmp)
-        vec.append(elem[1])
+            tmp.append(pow(float(elem[0]), j))
+        mat_A.append(tmp)
+        vec_b.append(float(elem[1]))
     #print("a = {}".format(mat))
     #print("b = {}".format(vec))
-    a_plus = calc_a_plus(matrix(mat))
-    tmp_res = matmul(a_plus, matrix(mat).transpose())
-    res = matmul(tmp_res, vec)
+    A_plus = calc_a_plus(array(mat_A))
+    tmp_res = matmul(A_plus, matrix(mat_A).transpose())
+    res = matmul(tmp_res, vec_b)#.tolist()
+    #print('res : {}'.format(res))
     return res
 
 def poly_fun(y_x,x):
+    """
+    Used to make weighted array into a function
+    :type y_x: array
+    :type x: array
+    """
     res = 0
-    for i in range(len(y_x)):
-        res += y_x.item(i)*pow(x,i)
-        #print(res)
-    return res
+    X = []
 
-def f_1(y_1,x):
-    return y_1.item(0) + y_1.item(1)*x
+    #print('res:{}'.format(res))
+    #print("len y_x", y_x.shape[1])
+    for i in range(y_x.shape[1]):
+        X.append(np.power(x,i))
 
-def f_2(y_2,x):
-    return y_2.item(0) + y_2.item(1)*x + y_2.item(2)*x**2
-
-def f_3(y_3,x):
-    return y_3.item(0) + y_3.item(1)*x + y_3.item(2)*x**2 +  y_3.item(3)*x**3
-
-def f_4(y_4,x):
-    return y_4.item(0) + y_4.item(1)*x + y_4.item(2)*x**2 +  y_4.item(3)*x**3 +  y_4.item(4)*x**4
-
-def f_5(y_5,x):
-    return y_5.item(0) + y_5.item(1)*x + y_5.item(2)*x**2 +  y_5.item(3)*x**3 +  y_5.item(4)*x**4 +  y_5.item(5)*x**5
-
-def f_6(y_6,x):
-    return y_6.item(0) + y_6.item(1)*x + y_6.item(2)*x**2 +  y_6.item(3)*x**3 +  y_6.item(4)*x**4 +  y_6.item(5)*x**5 + y_6.item(6)*x**6
-
-def f_7(y_7,x):
-    return y_7.item(0) + y_7.item(1)*x + y_7.item(2)*x**2 +  y_7.item(3)*x**3 +  y_7.item(4)*x**4 +  y_7.item(5)*x**5 + y_7.item(6)*x**6 + y_7.item(7)*x**7
-
-def f_8(y_8,x):
-    return y_8.item(0) + y_8.item(1)*x + y_8.item(2)*x**2 +  y_8.item(3)*x**3 +  y_8.item(4)*x**4 +  y_8.item(5)*x**5 + y_8.item(6)*x**6 + y_8.item(7)*x**7 + y_8.item(8)*x**8
-
-def f_9(y_9,x):
-    return y_9.item(0) + y_9.item(1)*x + y_9.item(2)*x**2 +  y_9.item(3)*x**3 +  y_9.item(4)*x**4 +  y_9.item(5)*x**5 + y_9.item(6)*x**6 + y_9.item(7)*x**7 + y_9.item(8)*x**8 + y_9.item(9)*x**9
+    #print("Y = ", y_x)
+    #print("X = ", matrix(X))
+    res = matmul(y_x, matrix(X))
+    #transform res into a list
+    Y = list()
+    for i in range(len(x)):
+        Y.append(res.item(i))
+    #print("Y= ", Y)
+    return Y
 
 # Error between prediction and trained data
-def E_1(y_k, xt, yt):
+def E(y_k, xt, yt):
     res = 0
     for i in range(len(xt)):
-        res += (f_1(y_k, xt[i]) - yt[i])**2
-        print(res)
+        res += (poly_fun(y_k, xt)[i]-yt[i])**2
     return res/2
 
-def E_2(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_2(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
+def calc_a_plus_regulator(mat, l):
+    """
+    Used to calcul the A+ = (A_T*A + l*I)^(-1) where x*=(A+)A_T.b
+    :type mat: matrix
+    """
+    #print("len(mat) = ", int(np.sqrt(mat.size)))
+    length = int(np.sqrt(mat.size))
+    reg = np.exp(l)*np.eye(length)
+    #print("reg = ", reg)
+    mat_trans = mat.transpose()
+    res_matmul = matmul(mat_trans, mat) + reg
+    a_plus_reg = inv(res_matmul)
+    return a_plus_reg
 
-def E_3(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_3(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
-
-def E_4(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_4(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
-
-def E_5(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_5(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
-
-def E_6(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_6(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
-
-def E_7(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_7(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
-
-def E_8(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_8(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
-
-def E_9(y_k, xt, yt):
-    res = 0
-    for i in range(len(xt)):
-        res += (f_9(y_k, xt[i]) - yt[i])**2
-        print(res)
-    return res/2
-
-###############################################################################
-# main
-###############################################################################
-
-# Create a vector y_sinoise of lenght 10 drawing a sinus with noise
-# It simulate trained data
-y_sinoise = []
-x_sinoise = []
-for i in range(10):
-    y_sinoise.append(np.sin(i/9*2*np.pi) + (np.random.random()-0.5))
-    x_sinoise.append(i/9*2*np.pi)
-#print(y_sinoise)
-#print(x_sinoise)
-
-###############################################################################
-# Linear regression
-m = []
-for i in range(10):
-    m.append([x_sinoise[i], y_sinoise[i]])
-
-#y_1 = poly_fun(line_regression(m,1))
-y_1 = line_regression(m,1)
-y_2 = line_regression(m,2)
-y_3 = line_regression(m,3)
-y_4 = line_regression(m,4)
-y_5 = line_regression(m,5)
-y_6 = line_regression(m,6)
-y_7 = line_regression(m,7)
-y_8 = line_regression(m,8)
-y_9 = line_regression(m,9)
-
-plt.figure(1)
-# Draw M = 1
-x = np.linspace(0, 2*np.pi, 100)
-#plt.plot(x, y_1.item(0) + y_1.item(1)*x, 'r', label='M = 1')
-plt.plot(x, f_1(y_1,x), 'r', label='M = 1')
-
-# Draw M = 2
-plt.plot(x, f_2(y_2,x), label='M = 2')
-#plt.plot(x,  poly_fun(y_2[0],x), label='M = 2')
-
-# Draw M = 3
-plt.plot(x, f_3(y_3,x), label='M = 3')
-
-# Draw M = 4
-plt.plot(x, f_4(y_4,x), label='M = 4')
-
-# Draw M = 9
-plt.plot(x, f_9(y_9,x), label='M = 9')
-
-# Draw a sinus
-x = np.linspace(0, 2*np.pi, 100)
-plt.plot(x, np.sin(x), '--g', label='sin')
-
-# Draw points of y_sinoise
-x = np.linspace(0, 2*np.pi, 10)
-plt.plot(x, y_sinoise, 'o')
-
-plt.xlabel('x')
-plt.ylabel('y')
-
-plt.title("Linear regression")
-
-###############################################################################
-# Error graph
-plt.figure(2)
-
-x_error = []
-for i in range(8):
-    x_error.append(i+1)
-
-y_error = [E_1(y_1, x_sinoise, y_sinoise), E_2(y_2, x_sinoise, y_sinoise), E_3(y_3, x_sinoise, y_sinoise), E_4(y_4, x_sinoise, y_sinoise), E_5(y_5, x_sinoise, y_sinoise), E_6(y_6, x_sinoise, y_sinoise), E_7(y_7, x_sinoise, y_sinoise), E_8(y_8, x_sinoise, y_sinoise), E_9(y_9, x_sinoise, y_sinoise)]
-
-for i in range(8):
-    y_error[i] = np.sqrt(2*y_error[i]/i)
-
-x = np.linspace(0, 9, 9)
-plt.plot(x, y_error, label='Training')
-
-plt.xlabel('M')
-plt.ylabel('Erms')
-
-plt.title("Error function")
-
-###############################################################################
-# Regularization
-plt.figure(3)
-# Draw M = 9
-plt.plot(x, f_9(y_9,x), label='M = 9')
-
-# Draw a sinus
-x = np.linspace(0, 2*np.pi, 100)
-plt.plot(x, np.sin(x), '--g', label='sin')
-
-plt.xlabel('x')
-plt.ylabel('y')
-
-plt.title("Regularization")
-
-plt.legend()
-
-plt.show()
+def regularization(mat_M, l):
+    """
+    Make calculation of linear regression adding a regularizator term l
+    :type main_mat: list
+    :type l: float
+    """
+    mat_A = []
+    vec_b = []
+    p=9
+    #for elem in range(len(mat_M)):
+    for elem in mat_M:
+        tmp = []
+        for j in range(p+1):
+            tmp.append(pow(float(elem[0]), j))
+        mat_A.append(tmp)
+        vec_b.append(float(elem[1]))
+    #print("a = {}".format(mat))
+    #print("b = {}".format(vec))
+    A_plus_regulator = calc_a_plus_regulator(array(mat_A), l)
+    tmp_res = matmul(A_plus_regulator, matrix(mat_A).transpose()) + A_plus_regulator
+    res = matmul(tmp_res, vec_b)#.tolist()
+    #print('res : {}'.format(res))
+    return res
---
+++ main.py
@@ -0,0 +1,118 @@
+from basics import *
+
+# Create a vector y_sinoise of lenght 10 drawing a sinus with noise
+# It simulate trained data
+y_sinoise = []
+x_sinoise = []
+for i in range(10):
+    y_sinoise.append(np.sin(i/9*2*np.pi) + (np.random.random()-0.5))
+    x_sinoise.append(i/9*2*np.pi)
+#print(y_sinoise)
+#print(x_sinoise)
+
+###############################################################################
+# Linear regression
+m = []
+for i in range(10):
+    m.append([x_sinoise[i], y_sinoise[i]])
+
+plt.figure(1)
+# Draw M = 1
+x = np.linspace(0, 2*np.pi, 100)
+#plt.plot(x, y_1.item(0) + y_1.item(1)*x, 'r', label='M = 1')
+plt.plot(x, poly_fun(line_regression(m,1),x), 'r', label='M = 1')
+
+# Draw M = 2
+plt.plot(x, poly_fun(line_regression(m,2),x), label='M = 2')
+#plt.plot(x,  poly_fun(y_2[0],x), label='M = 2')
+
+# Draw M = 3
+plt.plot(x, poly_fun(line_regression(m,3),x), label='M = 3')
+
+# Draw M = 4
+plt.plot(x, poly_fun(line_regression(m,4),x), label='M = 4')
+"""
+# Draw M = 5
+plt.plot(x, poly_fun(line_regression(m,5),x), label='M = 5')
+
+# Draw M = 6
+plt.plot(x, poly_fun(line_regression(m,6),x), label='M = 6')
+
+# Draw M = 7
+plt.plot(x, poly_fun(line_regression(m,7),x), label='M = 7')
+
+# Draw M = 8
+plt.plot(x, poly_fun(line_regression(m,8),x), label='M = 8')
+"""
+# Draw M = 9
+plt.plot(x, poly_fun(line_regression(m,9),x), label='M = 9')
+
+# Draw a sinus
+x = np.linspace(0, 2*np.pi, 100)
+plt.plot(x, np.sin(x), '--g', label='sin')
+
+# Draw points of y_sinoise
+x = np.linspace(0, 2*np.pi, 10)
+plt.plot(x, y_sinoise, 'o')
+
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.title("Linear regression")
+
+###############################################################################
+# Error graph
+#print("E = ", E(line_regression(m,1), x_sinoise, y_sinoise))
+
+plt.figure(2)
+
+x_error = []
+for i in range(8):
+    x_error.append(i+1)
+#np.array(x_error)
+#print("x_error = ", x_error)
+"""
+y_error = [E(line_regression(m,1), x_sinoise, y_sinoise), E(line_regression(m,2), x_sinoise, y_sinoise), E(line_regression(m,3), x_sinoise, y_sinoise), E(line_regression(m,4), x_sinoise, y_sinoise), E(line_regression(m,5), x_sinoise, y_sinoise), E(line_regression(m,6), x_sinoise, y_sinoise), E(line_regression(m,7), x_sinoise, y_sinoise), E(line_regression(m,8), x_sinoise, y_sinoise), E(line_regression(m,9), x_sinoise, y_sinoise)]
+"""
+y_error = []
+for i in range(8):
+    y_error.append(E(line_regression(m,i+1), x_sinoise, y_sinoise))
+
+# Make Erms
+for i in range(8):
+    y_error[i] = np.sqrt(2*y_error[i]/(i+1))
+
+plt.plot(x_error, y_error, label='Training')
+
+plt.xlabel('M')
+plt.ylabel('Erms')
+
+plt.title("Error function")
+
+
+###############################################################################
+# Regularization
+plt.figure(3)
+# Draw M = 9
+x = np.linspace(0, 2*np.pi, 100)
+plt.plot(x,  poly_fun(line_regression(m,9),x), label='M = 9')
+
+# Draw Regularized
+y_9_r = regularization(m, -1)
+#("reg vect = ", y_9_r)
+
+plt.plot(x,  poly_fun(y_9_r,x), label='Regularized')
+
+
+# Draw a sinus
+x = np.linspace(0, 2*np.pi, 100)
+plt.plot(x, np.sin(x), '--g', label='sin')
+
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.title("Regularization")
+
+plt.legend()
+
+plt.show()
